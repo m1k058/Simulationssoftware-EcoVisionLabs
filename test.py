9935fe58a64c3cfa98b82b8a2c72341fe1d7b255
m1k058 <michal.kos@haw-hamburg.de>
2025-10-13 18:44:46 +0000

    counter hinzugefügt

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 # Wie geht es dir?
+from time import sleep
+
+
 print("Hello Julian! Wie geht es dir?")
 eingabe = input()
 if eingabe == "gut":
     print("Das gut :)")
 else:
     print("Das nicht so gut...")
-
+print("3")
+sleep(1)
+print("2")
+sleep(1)
+print("1")
+sleep(1)
+print("0")
+sleep(1)
 print("\n\n\t****Programm wird beendet****\n")
