3ce880db75ce5b67c6a79866aedd89d1ce25755a
m1k058 <michal.kos@haw-hamburg.de>
2025-10-13 18:39:32 +0000

    ich habe etwas getestet

---
+++ test.py
@@ -0,0 +1,9 @@
+# Wie geht es dir?
+print("Hello Julian! Wie geht es dir?")
+eingabe = input()
+if eingabe == "gut":
+    print("Das gut :)")
+else:
+    print("Das nicht so gut...")
+
+print("\n\n\t****Programm wird beendet****\n")
