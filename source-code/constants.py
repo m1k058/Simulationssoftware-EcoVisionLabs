ca89a0168f10938d6e83a75675197c418c9cf192
m1k058 <michal.kos@haw-hamburg.de>
2025-10-19 15:42:12 +0200

    Add EnergySources enum with renewable and conventional energy types
    - Introduced an Enum class `EnergySources` to categorize various energy sources.
    - Each energy source includes a name, corresponding column label, and color code for visualization.
    - Supported energy types include Biomasse, Wasserkraft, Wind (Offshore), Wind (Onshore), Photovoltaik, Sonstige Erneuerbare, Kernenergie, Braunkohle, Steinkohle, Erdgas, Pumpspeicher, and Sonstige Konventionelle.

---
+++ source-code/constants.py
@@ -0,0 +1,64 @@
+from enum import Enum
+
+class EnergySources(Enum):
+    BIO = {
+        "name": "Biomasse",
+        "col": "Biomasse [MWh] Berechnete Auflösungen",
+        "color": "#00A51B"
+    }
+    WAS = {
+        "name": "Wasserkraft",
+        "col": "Wasserkraft [MWh] Berechnete Auflösungen",
+        "color": "#1E90FF"
+    }
+    WOF = {
+        "name": "Wind (Offshore)",
+        "col": "Wind Offshore [MWh] Berechnete Auflösungen",
+        "color": "#00BFFF"
+    }
+    WON = {
+        "name": "Wind (Onshore)",
+        "col": "Wind Onshore [MWh] Berechnete Auflösungen",
+        "color": "#00C4A3"
+    }
+    PV = {
+        "name": "Photovoltaik",
+        "col": "Photovoltaik [MWh] Berechnete Auflösungen",
+        "color": "#FFD700"
+    }
+    SOE = {
+        "name": "Sonstige Erneuerbare",
+        "col": "Sonstige Erneuerbare [MWh] Berechnete Auflösungen",
+        "color": "#ADFF2F"
+    }
+    KE = {
+        "name": "Kernenergie",
+        "col": "Kernenergie [MWh] Berechnete Auflösungen",
+        "color": "#800080"
+    }
+    BK = {
+        "name": "Braunkohle",
+        "col": "Braunkohle [MWh] Berechnete Auflösungen",
+        "color": "#362300"
+    }
+    SK = {
+        "name": "Steinkohle",
+        "col": "Steinkohle [MWh] Berechnete Auflösungen",
+        "color": "#1F1F1F"
+    }
+    EG = {
+        "name": "Erdgas",
+        "col": "Erdgas [MWh] Berechnete Auflösungen",
+        "color": "#5D5D5D"
+    }
+    PS = {
+        "name": "Pumpspeicher",
+        "col": "PS [MWh] Berechnete Auflösungen",  
+        "color": "#090085"
+    }
+    SOK = {
+        "name": "Sonstige Konventionelle",
+        "col": "Sonstige Konventionelle [MWh] Berechnete Auflösungen",
+        "color": "#A9A9A9"
+    }
+
---
+++ source-code/main.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from constants import EnergySources as ES
+
+
+DATA_PATH = r"raw-data\Realisierte_Erzeugung_2025_Jan-Juni_Stunde_test.csv" # Pfad zur CSV-Datei
+DATE_START = "01.01.2025 08:00" # Startdatum für die Filterung (MM.TT.JJJJ HH:MM)
+DATE_END = "01.05.2025 08:00" # Enddatum für die Filterung (MM.TT.JJJJ HH:MM)
+ENGY_SRC0 = ES.PV # Energiequelle für Stapelung
+ENGY_SRC1 = ES.SK # Energiequelle
+
+# Laden der CSV-Datei in ein DataFrame
+df = pd.read_csv(DATA_PATH, sep=';')
+
+# Start/End-Datum in datetime-Objekte umwandeln
+df["Datum von"] = pd.to_datetime(df["Datum von"], format="%d.%m.%Y %H:%M")
+df["Datum bis"] = pd.to_datetime(df["Datum bis"], format="%d.%m.%Y %H:%M")
+
+# Berechnung der mittleren Zeit für jeden Eintrag
+df["zeit"] = df["Datum von"] + (df["Datum bis"] - df["Datum von"]) / 2
+
+# Filtern der Daten
+df_filtered = df[(df["zeit"] >= DATE_START) & (df["zeit"] <= DATE_END)]
+
+# Konvertiere den String-Wert der .csv mit Komma in einen Float (deutsches Format zu US-Format (grrr!!! AMERIKANER!!!))
+df_filtered[ENGY_SRC0.value["col"]] = df_filtered[ENGY_SRC0.value["col"]].str.replace('.', '').str.replace(',', '.').astype(float)
+df_filtered[ENGY_SRC1.value["col"]] = df_filtered[ENGY_SRC1.value["col"]].str.replace('.', '').str.replace(',', '.').astype(float)
+
+
+# das Plotten der Daten
+plt.figure(figsize=(20,10))
+plt.bar(
+        df_filtered["zeit"],
+        df_filtered[ENGY_SRC0.value["col"]],
+        width=pd.Timedelta(hours=0.8),
+        align='center',
+        color=ENGY_SRC0.value["color"],
+        label=ENGY_SRC0.value["name"],
+        bottom=df_filtered[ENGY_SRC1.value["col"]] # Stapeln der Balken
+)
+
+plt.bar(
+        df_filtered["zeit"],
+        df_filtered[ENGY_SRC1.value["col"]],
+        width=pd.Timedelta(hours=0.8),
+        align='center',
+        color=ENGY_SRC1.value["color"],
+        label=ENGY_SRC1.value["name"]
+)
+
+
+plt.xlabel("Datum")
+plt.ylabel("Erzeugung [MWh]")
+plt.title("Stündliche Stromerzeugung " + ENGY_SRC0.value["name"] + " und " + ENGY_SRC1.value["name"] + " von " + DATE_START + " bis " + DATE_END)
+plt.legend()
+plt.grid(True)
+plt.gcf().autofmt_xdate()  # Datumsanzeige schräg stellen
+plt.show()
