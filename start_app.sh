9963d51e9b60308a2f69c4fde4053cfb8a664961
m1k058 <michal.kos@haw-hamburg.de>
2025-11-28 10:22:58 +0100

    Add requirements and scripts for virtual environment setup and Streamlit app launch

---
+++ start_app.sh
@@ -0,0 +1,22 @@
+
+VENV_DIR="venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Virtuelle Umgebung wird erstellt und Pakete installiert..."
+    python3 -m venv "$VENV_DIR"
+    source "$VENV_DIR/bin/activate"
+    
+    pip install -r requirements.txt
+else
+    echo "Virtuelle Umgebung wird aktiviert..."
+    source "$VENV_DIR/bin/activate"
+fi
+
+
+echo "Starte Streamlit-App..."
+
+
+streamlit run source_code/streamlit_ui.py
+-
+deactivate
+echo "Streamlit-App beendet."
